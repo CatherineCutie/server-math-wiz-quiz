3db6301c38b7e539207b38a18ed7c015219d4f7e
CatherineCutie <xuxa.balbuena7@gmail.com>
2024-09-09 13:24:37 +0800

    wew

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="icon" href="{{ asset('assets/imgs/logo/Untitled.png') }}" type="image/x-icon"/>
 
 
         <!-- Open Graph Meta Tags -->
